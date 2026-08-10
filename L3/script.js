bff901aac75a265bc76d61e13decd5fb6c1ab193
Eshtha <eshthabanga@gmail.com>
2026-08-10 21:55:39 +0530

    class assessment

---
+++ L3/script.js
@@ -0,0 +1,20 @@
+// function add(a,b){
+//     return a+b;
+// }
+// console.log(add(5,3));
+
+// -----common js-------
+// const maths = require('./maths.js');
+
+// console.log(maths.add(5,3));
+// console.log(maths.subtract(5,3));
+
+// console.log("helllo world");
+
+
+// -----es method----
+
+import {add,subtract} from './maths.js';
+
+console.log(add(5,3));
+console.log(subtract(5,3));
