0a4517ad61a1ab618796db57db69a24dce47e40f
Eshtha <eshthabanga@gmail.com>
2026-08-13 11:56:39 +0530

    Update existing file

--- L5/coremodules.js
+++ L5/coremodules.js
@@ -1,7 +1,27 @@
 const os=require('os');
+const path=require('path');
+const fs=require('fs');
 
 // console.log(os.platform())
 // console.log(os.arch())
+// console.log(os.totalmem()/(1024*1024*1024) + "GB");
+// console.log(os.freemem()/(1024*1024) + "MB");
 
-console.log(os.totalmem()/(1024*1024*1024) + "GB");
-console.log(os.freemem()/(1024*1024) + "MB");
+// const filePath=path.join(__dirname,"coremodules.js");
+// console.log(filePath);
+
+// const file= "coremodules.js";
+// console.log(path.basename(file));
+// console.log(path.extname(file));
+// console.log(path.dirname(file));
+
+// const data=fs.readFileSync(path.join("sample.txt","utf-8");
+// console.log(data);
+
+// fs.readFile(path.join("sample.txt"),"utf-8",(err,data)=>{
+//     if(err) throw err;
+//     console.log(data);
+//     else{
+//         console.log("File read successfully");
+//     }
+// }   
