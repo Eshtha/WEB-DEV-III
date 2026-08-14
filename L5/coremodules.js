1f27914a5427754ae2e1b3fe8fedb1f0c1e8a08f
Eshtha <eshthabanga@gmail.com>
2026-08-14 23:22:51 +0530

    L6-7

--- L5/coremodules.js
+++ L5/coremodules.js
@@ -1,6 +1,8 @@
 const os=require('os');
 const path=require('path');
 const fs=require('fs');
+const crypto=require('crypto');
+const dns=require('dns');
 
 // console.log(os.platform())
 // console.log(os.arch())
@@ -25,3 +27,42 @@ const fs=require('fs');
 //         console.log("File read successfully");
 //     }
 // }   
+
+// fs.writeFile(path.join(".\sample.txt"),"Hello, this is the updated data",(err)=>{
+//     if(err)console.log(err);
+// }
+
+// fs.appendFileSync("sample.txt", "\nThis is the appended data");
+
+// fs.unlinkSync("file.txt");
+
+// fs.renameSync("sample.txt","newfile.txt");
+
+// fs.copyFileSync("newfile.txt","copyfile.txt");
+
+// const password="mysecretpassword";
+
+//same hash for same password
+// const hash=crypto.createHash('sha256').update(password).digest('hex');
+// console.log(hash);
+
+// const salt=crypto.randomBytes(16).toString('hex');
+// const hash=crypto.createHmac('sha512', salt).update(password).digest('hex');
+// console.log(hash);
+
+// dns.lookup('www.google.com',(err,address,family)=>{
+//     if(err) {
+//       console.log(err);  
+//     } else {
+//         console.log(address);
+//         console.log(family);
+//     }   
+// })
+
+// dns.reverse('8.8.8.8', (err, hostnames) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(hostnames);
+//     }
+// });
